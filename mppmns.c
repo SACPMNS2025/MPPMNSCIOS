4e21fe2201cd446a3a0c5c909e3afd539be4efd6
SACPMNS2025 <onilateurbackup@gmail.com>
2025-01-27 10:56:27 +0100

    Update mppmns.c

--- mppmns.c
+++ mppmns.c
@@ -326,7 +326,7 @@ int main(void)
 	time_t seed;
 	srand((unsigned) (time(&seed)));
 	
-	printf("cycles: %ld\n", do_bench(pmns_montg_mult,N>40 ? 3 : 33));
+	printf("This work s = %d: %ld\n", NBCHUNKS, do_bench(pmns_montg_mult,N>40 ? 3 : 33));
 	
 	FILE *fpointer = fopen("log", "w+");
 	fclose(fpointer);
